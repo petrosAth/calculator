f7387f18e981277999d7258f05eb4dbeca3a77f5
petrosAth <develop@athanasoulis.me>
2022-10-27 14:44:53 +0300

    Add operations' functions

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -5,3 +5,20 @@ let operation = {
   },
   sign: null,
 };
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function substract(num1, num2) {
+  return num1 - num2;
+}
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function divide(num1, num2) {
+  const msg = "Can't divide with zero!";
+  return num2 === 0 ? msg : num1 / num2;
+}
